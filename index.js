35fbf58d000523e405bc8c4630f2d64f6cec7b10
Bekah Hawrot Weigel <rhawrot@gmail.com>
2017-12-28 17:20:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {key: 'value'}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {prop: 1, key: ['value'], prop2: 2 })
+
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key)
+{
+
+ 
+}
