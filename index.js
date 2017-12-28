4f72caf15962a274979acb6df332ffcdd0a16774
Bekah Hawrot Weigel <rhawrot@gmail.com>
2017-12-28 17:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key) {
-  Object.assign({}, object)
+ var newObj = Object.assign({}, object)
+ 
+ delete newObject[key]
   return object
   
  
